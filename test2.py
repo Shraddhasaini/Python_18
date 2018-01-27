68f6147f52c42cef4a4d48f24194b57d97e8b2a9
Shraddha Saini <shraddhasaini99@gmail.com>
2018-01-27 10:00:10 +0530

    [101]initial commit

---
+++ test2.py
@@ -0,0 +1,12 @@
+print "shraddha"
+a = 5
+b = 4
+aa = "9"
+print a, b, aa
+print a+b
+print aa + str(b)
+print int(aa) + b
+c = aa + str(b)
+print c
+d = str(int(aa + str(b)) + int(aa)) + str(b)
+print int(d)+int(d)*int(d)^2*3/4
