ddf8a7cf7be4c9b08cf491e1871f96243fe9a37c
saurabh <shraddhasaini99@gmail.com>
2019-02-05 21:21:16 +0530

    [level101.1]capitalize

--- oldmcd.py
+++ oldmcd.py
@@ -1,8 +1,6 @@
 #capitalizes the first and fourth letters of a name
 def oldmcd(name):
-    first = name[0].upper()
-    inbetween = name[1:3]
-    fourth = name[3].upper()
-    rest = name[4:]
-    return first + inbetween + fourth + rest
+    firstpart = name[:3].capitalize()
+    secondpart = name[3:].capitalize()
+    return firstpart + secondpart
 print(oldmcd("macdonald"))
